b3aa13ae1862881ec49ff0d7781264b51de67312
kevin <kevinlu113@gmail.com>
2021-01-08 17:27:54 -0500

    handle the click event and print out which dot is clicked

--- quiz.py
+++ quiz.py
@@ -14,23 +14,32 @@ class Quiz:
 
 
 
-# list = []
-
-# list.append( Quiz('Which of the following is a consequence of overdrawing your checking account?', "D", dict([('A', 'Bounced check fee from the store'), ('B', 'Overdraft fee from your bank'), ('C', 'Stress from money mismanagement'), ('D', 'All of the above')]), 'Easy') )
-# list.append( Quiz('Rent is a:', "A", dict([('A', 'Fixed expense'), ('B', 'Variable expense'), ('C', 'Discretionary Expense'), ('D', 'Intermittent expense')]), 'Easy') )
-# list.append( Quiz('Groceries are a:', "C", dict([('A', 'Fixed expense'), ('B', 'Intermittent expense'), ('C', 'Variable expense'), ('D', 'Discretionary expense')]), 'Easy'))
 
 
-quiz1 = Quiz('Which of the following is a consequence of overdrawing your checking account?', "D", dict([('A', 'Bounced check fee from the store'), ('B', 'Overdraft fee from your bank'), ('C', 'Stress from money mismanagement'), ('D', 'All of the above')]), 'Easy')
-print(quiz1.question)
-for letter, text in quiz1.choices.items():  
-    print("{} ({})".format(letter, text))
-answer = input("Choose your answer:")
-question1 = quiz1.check_answer(answer)
-print(question1)
+quizList = []
 
-        
-    
+quizList.append( Quiz('Which of the following is a consequence of overdrawing your checking account?', "D", dict([('A', 'Bounced check fee from the store'), ('B', 'Overdraft fee from your bank'), ('C', 'Stress from money mismanagement'), ('D', 'All of the above')]), 'Easy') )
+quizList.append( Quiz('Rent is a:', "A", dict([('A', 'Fixed expense'), ('B', 'Variable expense'), ('C', 'Discretionary Expense'), ('D', 'Intermittent expense')]), 'Easy') )
+quizList.append( Quiz('Groceries are a:', "C", dict([('A', 'Fixed expense'), ('B', 'Intermittent expense'), ('C', 'Variable expense'), ('D', 'Discretionary expense')]), 'Easy'))
+
+for quiz in quizList:
+    print(quiz.question)
+    for letter, text in quiz.choices.items():  
+        print("{} ({})".format(letter, text))
+    answer = input("Choose your answer:")
+    question1 = quiz.check_answer(answer)
+    print(question1)
+
+
+# quiz1 = Quiz('Which of the following is a consequence of overdrawing your checking account?', "D", dict([('A', 'Bounced check fee from the store'), ('B', 'Overdraft fee from your bank'), ('C', 'Stress from money mismanagement'), ('D', 'All of the above')]), 'Easy')
+# print(quiz1.question)
+# for letter, text in quiz1.choices.items():  
+#     print("{} ({})".format(letter, text))
+# answer = input("Choose your answer:")
+# question1 = quiz1.check_answer(answer)
+# print(question1)
+
+
 
 
 
